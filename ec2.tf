36d60d47ef045300e9a5fa2bb758549262ac22d9
Kristof-ay <kris200036@gmail.com>
2025-03-11 16:20:17 +0100

    making ec2 with jenkins for ci/cd django project

---
+++ ec2.tf
@@ -0,0 +1,40 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "jenkins_instance"
+
+  instance_type          = "t2.small"
+  key_name               = "jenkins"
+  monitoring = true
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  subnet_id              = data.aws_subnet.subnet.id
+
+  tags = {
+    Environment = "dev"
+  }
+  user_data = <<-EOF
+        #!/bin/bash
+        set -e
+        wget -O /usr/share/keyrings/jenkins-keyring.asc \
+        https://pkg.jenkins.io/debian/jenkins.io-2023.key
+        echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+        https://pkg.jenkins.io/debian binary/ | sudo tee \
+        /etc/apt/sources.list.d/jenkins.list > /dev/null
+        apt-get update
+        apt-get install -y openjdk-11-jdk jenkins
+        systemctl start jenkins
+        systemctl enable jenkins
+        systemctl status jenkins
+        cat /var/lib/jenkins/secrets/initialAdminPassword' >> /var/log/jenkins-password.log
+  EOF
+}
+
+data "aws_subnet" "subnet" {
+  filter{
+    name = "vpc-id"
+    values = [data.aws_default.id]
+  }
+ 
+
+}
+

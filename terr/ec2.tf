b17be24d15a5c9ba1ee1d8f8a2e829acff5c5d69
Kristof-ay <kris200036@gmail.com>
2025-03-17 20:10:07 +0100

    Main

--- terr/ec2.tf
+++ terr/ec2.tf
@@ -14,9 +14,29 @@ module "ec2_instance" {
     Environment = "dev"
   }
   user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello bello" >  /home/hello.txt
-                EOF
+                 #!/bin/bash
+        set -e
+        apt-get update
+        apt-get install -y openjdk-21-jdk 
+
+        wget -O /usr/share/keyrings/jenkins-keyring.asc \
+        https://pkg.jenkins.io/debian/jenkins.io-2023.key
+
+        echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+        https://pkg.jenkins.io/debian binary/ | sudo tee \
+        /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+        apt-get update
+
+        apt-get install -y jenkins
+
+        systemctl start jenkins
+
+        systemctl enable --now jenkins
+
+        cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
 }
 
 data "aws_subnet" "default" {
